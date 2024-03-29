6089c4d6e1aab3abe304991d6387711133c5e4bc
itsmemasir <100307719+itsmemasir@users.noreply.github.com>
2024-03-30 00:58:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -64,20 +64,4 @@ if "Appointment" in result:
         smtp.sendmail(email_sender, email_receiver, em.as_string())
 
 else:
-    email_sender = "theflashvszoom28@gmail.com"
-    email_password = "slwa zrji mgsz rpzm"
-    email_receiver = "masirthegreat28@gmail.com" #najaf.ahmed@bellevuecollege.edu
-
-    subject = "Biometrics Appointment"
-    body = result
-    em = EmailMessage()
-    em["Form"] = email_sender
-    em["To"] = email_receiver
-    em["Subject"] = subject
-    em.set_content(body)
-
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
+    print("nothing yet")
