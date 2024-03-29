4305c2e8ffe2012b275296eb1ce2d20094023eaa
itsmemasir <100307719+itsmemasir@users.noreply.github.com>
2024-03-30 00:55:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 import os
-
+from email.message import EmailMessage
+import ssl #for security
+import smtplib
 import requests
 
 try:
@@ -43,10 +45,6 @@ except:
     print("error in finding appt status")
 
 if "Appointment" in result:
-    from email.message import EmailMessage
-    import ssl #for security
-    import smtplib
-
     email_sender = "theflashvszoom28@gmail.com"
     email_password = "slwa zrji mgsz rpzm"
     email_receiver = "najaf.ahmed@bellevuecollege.edu" #najaf.ahmed@bellevuecollege.edu
@@ -66,4 +64,20 @@ if "Appointment" in result:
         smtp.sendmail(email_sender, email_receiver, em.as_string())
 
 else:
-    print("oopsie, appointment not available") 
+    email_sender = "theflashvszoom28@gmail.com"
+    email_password = "slwa zrji mgsz rpzm"
+    email_receiver = "masirthegreat28@gmail.com" #najaf.ahmed@bellevuecollege.edu
+
+    subject = "Biometrics Appointment"
+    body = result
+    em = EmailMessage()
+    em["Form"] = email_sender
+    em["To"] = email_receiver
+    em["Subject"] = subject
+    em.set_content(body)
+
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL("smtp.gmail.com", 465, context=context) as smtp:
+        smtp.login(email_sender, email_password)
+        smtp.sendmail(email_sender, email_receiver, em.as_string())
